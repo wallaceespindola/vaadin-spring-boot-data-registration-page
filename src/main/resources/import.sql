04d7ee5d15000e871faff7b6ed83e38b6a72e379
Wallace Espindola <wallace.espindola@gmail.com>
2023-07-01 12:43:26 +0200

    Adding initialization data

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+-- Insert test data into REGISTRATION table
+INSERT INTO REGISTRATION (ID, NAME, EMAIL, SHIRT_SIZE) VALUES (10, 'John Doe', 'john.doe@example.com', 'M');
+INSERT INTO REGISTRATION (ID, NAME, EMAIL, SHIRT_SIZE) VALUES (20, 'Jane Smith', 'jane.smith@example.com', 'S');
+INSERT INTO REGISTRATION (ID, NAME, EMAIL, SHIRT_SIZE) VALUES (30, 'Bob Johnson', 'bob.johnson@example.com', 'L');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+-- DDL to create REGISTRATION table
+CREATE TABLE REGISTRATION (
+                              ID INT PRIMARY KEY,
+                              NAME VARCHAR(100) NOT NULL,
+                              EMAIL VARCHAR(100) NOT NULL,
+                              SHIRT_SIZE VARCHAR(5)
+);
